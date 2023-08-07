d76b168dc5224f65f3886ba15547411c7df9e886
CodyIsTheSenate <mathieuman78@gmail.com>
2023-08-07 22:04:40 +0200

    Add swagger

--- back_node/api/query.js
+++ back_node/api/query.js
@@ -1,5 +1,38 @@
 const use = require("../universalSentenceEncoder/index");
 
+/**
+ * @swagger
+ * /api/query/:
+ *   get:
+ *     summary: Ask something to Kara
+ *     tags: []
+ *     parameters:
+ *     - name: "query"
+ *       in: "query"
+ *       description: "The question"
+ *       required: true
+ *       type: string
+ *     responses:
+ *       200:
+ *         description: "Get all ticlets data from a user"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: "string"
+ *                   description: Phrase from Kara (always present)
+ *                 similarity:
+ *                   type: number
+ *                   description: Value of similarity between the answer and the query or bestPhrase (if the question is invalid)
+ *                 bestPhrase:
+ *                   type: "string"
+ *                   description: The sentence that comes closest (invalid question)
+ *               example:
+ *                 result: Bonjour je suis Kara
+ */
+
 module.exports.start = (app) => {
   app.get("/api/query", async function (req, res) {
     const result = await use.query(req.query.query.toLowerCase());
--- back_node/index.js
+++ back_node/index.js
@@ -1,13 +1,48 @@
 const express = require("express");
-const use = require("./universalSentenceEncoder/index");
 const app = express();
+require("dotenv").config();
+const use = require("./universalSentenceEncoder/index");
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 require("./api/index").startApi(app);
 
+if (process.env.SHOWSWAGGER === "true") {
+  const swaggerUI = require("swagger-ui-express");
+  const swaggerJsDoc = require("swagger-jsdoc");
+  const servers = [
+    {
+      url: `http://localhost:${port}`,
+    },
+  ];
+  if (process.env.PROD_URL)
+    servers.push({
+      url: process.envPROD_URL,
+    });
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "KarAssistant API",
+        version: require("./package.json").version,
+        description:
+          "Hello and welcome to the API documentation of the KarAssistant.\n" +
+          "This project is an personnal assistant. You can ask a question and Kara will answer you\n",
+      },
+      servers,
+    },
+    apis: ["./api/*.js", "./api/*/*.js", "./api/*/*/*.js"],
+  };
+
+  const specs = swaggerJsDoc(options);
+
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+}
+
 async function start() {
   await use.start();
-  app.listen(3000);
-  console.log("\n\x1b[33mApp is listening port : 3000\x1b[0m\n");
+  app.listen(port);
+  console.log(`\n\x1b[33mApp is listening port : ${port}\x1b[0m`);
+  if (process.env.SHOWSWAGGER) console.log(`Documentation available here : http://localhost:${port}/api-docs\n`);
 }
 
 start();
