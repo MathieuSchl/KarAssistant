23917474bd1b2a1a587371e20acd3a2ba89cf638
CodyIsTheSenate <mathieuman78@gmail.com>
2023-08-07 23:11:00 +0200

    Calculate time of encoding

--- back_node/universalSentenceEncoder/index.js
+++ back_node/universalSentenceEncoder/index.js
@@ -21,8 +21,48 @@ function progressLog(text) {
   lineProgress = text;
 }
 
+function dateDiff(dateStart, dateEnd) {
+  const timeDifference = dateEnd - dateStart;
+
+  const seconds = Math.floor(timeDifference / 1000) % 60;
+  const minutes = Math.floor(timeDifference / (1000 * 60)) % 60;
+  const hours = Math.floor(timeDifference / (1000 * 60 * 60)) % 24;
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24)) % 30; // Assuming a month has 30 days for simplicity
+  const months = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 30)) % 12;
+  const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365));
+
+  let result = "";
+
+  if (years > 0) {
+    result += `${years} year${years > 1 ? "s" : ""} `; //Yes, you have to worry if the encoding lasts more than a year
+  }
+
+  if (months > 0) {
+    result += `${months} month${months > 1 ? "s" : ""} `; //Yes, you have to worry if the encoding lasts more than a month
+  }
+
+  if (days > 0) {
+    result += `${days} day${days > 1 ? "s" : ""} `; //Yes, you have to worry if the encoding lasts more than a day
+  }
+
+  if (hours > 0) {
+    result += `${hours} hour${hours > 1 ? "s" : ""} `; //Yes, you have to worry if the encoding lasts more than a hour
+  }
+
+  if (minutes > 0) {
+    result += `${minutes} minute${minutes > 1 ? "s" : ""} `;
+  }
+
+  if (seconds > 0) {
+    result += `${seconds} second${seconds > 1 ? "s" : ""} `;
+  }
+
+  return result.trim();
+}
+
 module.exports.start = async () => {
   await start();
+  const dateStart = new Date();
   const files = fs.readdirSync(__dirname + "/../skills", { withFileTypes: true });
   const folders = files.filter((file) => file.isDirectory()).map((file) => file.name);
 
@@ -47,7 +87,9 @@ module.exports.start = async () => {
   process.stdout.clearLine(0);
   process.stdout.cursorTo(0);
   process.stdout.write(`\x1b[32m${length}/${length} 100.00%`);
-  console.log("\n\x1b[33mEncode sentences finished\x1b[0m\n");
+  const dateEnd = new Date();
+  const elapsedTimeText = dateDiff(dateStart, dateEnd);
+  console.log(`\n\x1b[33mEncode sentences finished in \x1b[35m${elapsedTimeText}\x1b[0m\n`);
 };
 
 module.exports.query = async ({ query }) => {
