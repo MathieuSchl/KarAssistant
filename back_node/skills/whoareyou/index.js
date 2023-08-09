5066895e31ac8a3c94133f414499b602cec1894f
CodyIsTheSenate <mathieuman78@gmail.com>
2023-08-09 15:22:32 +0200

    Update fr phrase response

--- back_node/skills/whoareyou/index.js
+++ back_node/skills/whoareyou/index.js
@@ -24,9 +24,9 @@ module.exports.execute = (data) => {
       return `Hi my name is Kara. I'm a personal assistant developed by Cody and Caz. Ask me a question and I'll answer it.`;
 
     case "fr":
-      return `Mon nom est Kara. Je suis un assistant personnel dévellopé par Cody et Caz. Posez moi, une question et j'y répondrais.`;
+      return `Mon nom est Kara. Je suis un assistant personnel dévellopé par Cody et Caz. Posez-moi une question et j'y répondrais.`;
 
     default:
-      return `Undefined language for whoiam`;
+      return `Undefined language for whoareyou`;
   }
 };
