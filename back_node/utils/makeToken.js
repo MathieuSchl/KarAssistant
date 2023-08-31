690b7dc6ee29c0deb7d49b7155e2f53b06c261e2
CodyIsTheSenate <mathieuman78@gmail.com>
2023-08-31 08:23:22 +0000

    Improve make token

--- back_node/utils/makeToken.js
+++ back_node/utils/makeToken.js
@@ -1,20 +1,29 @@
 const sha1 = require("crypto-js/sha1");
 const fs = require("fs");
 
-module.exports.generateToken = generateToken;
-function generateToken(params) {
-  const forceToken = params ? params.token : false;
-  const dateTime = `${new Date().getTime()} `;
-
-  const array = dateTime.split("");
+function suffleString(string){
+  const array = string.split("");
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 
-  const dateTimeShuffled = array.join("");
-  const token = forceToken ? forceToken : sha1(dateTimeShuffled).toString();
-  if (fs.existsSync(__dirname + "/../data/sessions/" + token + ".json"))
+  const result = array.join("");
+  return result;
+}
+
+module.exports.generateToken = generateToken;
+function generateToken(params) {
+  const forceToken = params ? params.token : false;
+  const dateTime = `${new Date().getTime()} `;
+
+  const token1 = suffleString(dateTime);
+  const token2 = sha1(token1).toString();
+  const token3 = suffleString(token2);
+  const token4 = sha1(token3).toString();
+  
+  const finalToken = forceToken ? forceToken : token4;
+  if (fs.existsSync(__dirname + "/../data/sessions/" + finalToken + ".json"))
     return generateToken();
-  return token;
+  return finalToken;
 }
